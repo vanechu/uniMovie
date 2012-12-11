aafe4baa95324c39db071b239e489ba30648f671
VaneChu <i@mabudao.com>
2012-12-11 19:46:20 +0800

    page injection

--- background.js
+++ background.js
@@ -17,39 +17,44 @@ var setBadgeText = function(text) {
     chrome.browserAction.setBadgeText({ text: text });
 };
 
-// Called when the url of a tab changes.
 var checkForValidUrl = function(tabId, changeInfo, tab) {
     if (tab.url.indexOf('douban.com') > 0) {
         console.log("Find");
     }
 };
 
-var autoDetect =function(imdbNum) {
+var getMovie =function(imdbNum) {
     $.getJSON('http://192.168.246.69/imdb/'+imdbNum, function(data, status, xhr){
         if( xhr.status == 200 ){
-            console.log(data.MovieName);
+            sendMessage(data);
         }else{ 
-        //error - check out the values for only in chrome for 'console'
-        console.log(xhr.status);
-        console.log(xhr.response);
-        console.log(xhr.responseText)
-        console.log(xhr.statusText);
+            console.log(xhr.status);
+            console.log(xhr.response);
+            console.log(xhr.responseText)
+            console.log(xhr.statusText);
         }
     });
 }
 
+var sendMessage =  function (MovieInfo){
+    chrome.tabs.sendMessage(parseInt(localStorage.tabId), MovieInfo, function(response) {
+        console.log(response.farewell);
+    });
+    console.log("dadasdas"+localStorage.tabId);
+}
 
-console.log("init");
-//autoDetect();
+
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if(changeInfo.status === "complete"){
-      if(tab.url.search(url) !== -1) {
-        chrome.tabs.executeScript(tabId, { file: "jquery.js" }, function() {
+    if(changeInfo.status === "complete"){
+        if(tab.url.search(url) !== -1) {
+            chrome.tabs.executeScript(tabId, { file: "jquery.js" }, function() {
             chrome.tabs.executeScript(tabId, { file: "page.js" });
-        });
+            });
+            localStorage.tabId = tabId;
+            console.log("sadasd"+localStorage.tabId);
+        }
     }
-}
 });
 
 chrome.extension.onMessage.addListener(
@@ -57,7 +62,7 @@ chrome.extension.onMessage.addListener(
     console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
     if (request.imdb){
         console.log("Receive: "+request.imdb);
-        autoDetect(request.imdb);
+        getMovie(request.imdb);
     }
     sendResponse({farewell: "goodbye"});
 });
--- page.js
+++ page.js
@@ -2,12 +2,17 @@ var imdbNum = $('a:contains(tt)').text();
 
 console.log(imdbNum);
 
-$.getJSON("http://192.168.246.69/imdb/"+imdbNum,
-	function(movieInfo){
-		console.log(movieInfo.movieName);
-    });
-
 
 chrome.extension.sendMessage({imdb:imdbNum}, function(response){
 	console.log(response.farewell);
 })
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+    if (request.greeting == "hello"){
+    	sendResponse({farewell: "goodbye"});
+    }
+    console.log(request.MovieName);
+    $("a:contains(tt)").after("<br> <span class='pl'>下载地址:</span><a href='"+request.FtpPath+"' target='_blank' rel='nofollow'>"+request.FtpPath+"</a>");
+
+});
